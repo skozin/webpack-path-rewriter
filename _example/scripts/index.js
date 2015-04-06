90f4ac6d2b1e7dcac843b84e5ebe88e988484b47
Sam Kozin <i.a.m@bk.ru>
2015-04-07 02:59:52 +0300

    Add basic example

---
+++ _example/scripts/index.js
@@ -0,0 +1,4 @@
+require('../index.jade')
+require('../styles/index.styl')
+
+console.log('Hi everyone!')
---
+++ _example/webpack.config.js
@@ -0,0 +1,32 @@
+var ExtractTextPlugin = require('extract-text-webpack-plugin'),
+    PathRewriterPlugin = require('webpack-path-rewriter')
+
+module.exports = {
+  entry: {
+    app: './scripts/index'
+  },
+  output: {
+    path: '_dist',
+    filename: 'app-[chunkhash].js',
+    publicPath: '/public/path/'
+  },
+  module: {
+    loaders: [{
+      test: /[/]images[/]/,
+      loader: 'file?name=[path][name]-[hash].[ext]'
+    }, {
+      test: /[.]styl$/,
+      loader: ExtractTextPlugin.extract('css?sourceMap!stylus?sourceMap')
+    }, {
+      test: /[.]jade$/,
+      loader: PathRewriterPlugin.rewriteAndEmit({
+        name: '[path][name].html',
+        loader: 'jade-html?' + JSON.stringify({ pretty: true })
+      })
+    }]
+  },
+  plugins: [
+    new ExtractTextPlugin('app-[contenthash].css', { allChunks: true }),
+    new PathRewriterPlugin()
+  ]
+}
